2ae2ab34d42443258b16e290961571f5fc9f1a40
Fabian Sigfridsson <69erik355@gmail.com>
2024-01-18 11:07:39 +0100

    ✨

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,31 @@ router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome', part: story.parts[0] })
 })
 
+router.get('/story', function (req, res) {
+  const part = story.parts.find((part) => part.id === 1)
+  res.render('story.njk', { 
+    username: req.session.username,
+    title: part.name, 
+    part: part })
+})
+
+router.post('/username', function (req, res) {
+  req.session.username = req.body.username
+  console.log(req.session.username)
+  res.redirect('/story/0')
+})
+
 router.get('/story/:id', function (req, res) {
-  const part = story.parts.find((part) => part.id === parseInt(req.params.id))
+  let part = story.parts.find((part) => part.id === parseInt(req.params.id))
   if (!part) {
     res.status(404).render('404.njk', { title: '404' })
     return
   }
+  const text = part.text.replace('[USERNAME]', req.session.username);
+  for (let option of part.options) {
+    option.text = option.text.replace('[USERNAME]', req.session.username);
+  }
+  part = { ...part, text: text }
   res.render('part.njk', { title: part.name, part: part })
 })
 
